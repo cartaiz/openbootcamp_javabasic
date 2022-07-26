48aa2ac14d3786ed8c121c8e0944b8045e8b1f23
cartaiz <109952461+cartaiz@users.noreply.github.com>
2022-07-26 11:38:14 +0200

    Add files via upload

---
+++ EjercicioEstructurasControl.java
@@ -0,0 +1,15 @@
+package com.example.estructurascontrol;
+/**
+ * Crear un bucle que permita concatenar textos e imprima el resultado final por consola
+ */
+public class EjercicioEstructurasControl {
+
+    public static void main(String[] args) {
+
+        String[] nombres = {"Carlos", "Juan", "Mar", "Sandra", "Abril"};
+
+        for(String nombre : nombres) {
+            System.out.println("Su nombre es " + nombre);
+        }
+    }
+}
